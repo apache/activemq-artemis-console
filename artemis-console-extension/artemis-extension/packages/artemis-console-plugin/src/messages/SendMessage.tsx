d55e295f6da5036a5a84c57a7a57d563c610c5d3
Vilius Šumskas <vilius.sumskas@rivile.lt>
2025-07-22 10:48:32 +0300

    ARTEMIS-5569 Include Monaco editor scripts locally

--- artemis-console-extension/artemis-extension/packages/artemis-console-plugin/src/messages/SendMessage.tsx
+++ artemis-console-extension/artemis-extension/packages/artemis-console-plugin/src/messages/SendMessage.tsx
@@ -16,6 +16,7 @@
  */
 import React, { FormEvent, useRef, useState } from 'react'
 import * as monacoEditor from 'monaco-editor'
+import { monacoLoader } from '@monaco-editor/react'
 import xmlFormat from 'xml-formatter'
 
 import {
@@ -44,6 +45,8 @@ import { CodeEditor, Language } from '@patternfly/react-code-editor'
 import { eventService } from '@hawtio/react'
 import { artemisService } from '../artemis-service'
 import { Message } from './MessageView'
+
+monacoLoader.config({ monacoEditor })
 
 type SendBodyMessageProps = {
   onBodyChange: (body: string) => void
